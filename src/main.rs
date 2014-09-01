29cb67e999c428051141f1ecd6c53445b91f1cd3
da4c30ff <da4c30ff@ssl-mail.com>
2014-09-01 17:57:50 +0300

    Removed commented out code.

--- src/main.rs
+++ src/main.rs
@@ -41,16 +41,6 @@ fn main() {
         Some(verts) => verts.verts,
         None => fail!("Couldn't generate chunk from map!")
     };
-
-//     let mesh = device.create_mesh(vertex_data);
-//     let index_data: Vec<u8> = vec![
-//         0, 1, 2, 2, 3, 0, // top
-//         4, 5, 6, 6, 7, 4, // bottom
-//         8, 9, 10, 10, 11, 8, // right
-//         12, 13, 14, 14, 16, 12, // left
-//         16, 17, 18, 18, 19, 16, // front
-//         20, 21, 22, 22, 23, 20, // back
-//     ];
     
     let (mut device, frame) = window.gfx();
     let mut renderer = Renderer::new(device, frame);
